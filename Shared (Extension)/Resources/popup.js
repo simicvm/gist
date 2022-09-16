2bb5eec3e51cfb3384cc620996a5f8fe9c568c6f
Marko Simic <simic10@gmail.com>
2022-09-16 13:26:16 +0900

    add button change layout

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,22 @@
-console.log("Hello World!", browser);
+function printConsole() {
+    console.log("Hello World!");
+}
+
+function handleResponse(message) {
+    console.log(`Message from the background script: ${message}`);
+}
+
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function notifyBackgroundPage(e) {
+  const sending = browser.runtime.sendMessage({
+    greeting: "hello",
+  });
+  sending.then(handleResponse, handleError);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("scanPage").addEventListener("click", notifyBackgroundPage);
+});
