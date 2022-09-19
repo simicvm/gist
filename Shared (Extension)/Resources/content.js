0bc20aba4b051870fd180a599a86ad976c806497
Marko Simic <simic10@gmail.com>
2022-09-19 18:13:13 +0900

    Add Readability functionality to the Web Extension
    - Send command via popup button
    - Load Readability through Skypack
    - Extract text with Readability

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -3,5 +3,39 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 });
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    if (!sender.tab) {
+        console.log("Received request: ", request);
+
+        const documentClone = document.cloneNode(true);
+        import('https://cdn.skypack.dev/@mozilla/readability')
+        .then((module) => {
+            if (module.isProbablyReaderable(documentClone)) {
+                console.log("Page is readable!");
+                const article = new module.Readability(documentClone).parse();
+                console.log(article.title);
+                console.log(article.content)
+                console.log(article.textContent)
+            } else {
+                console.log("Page is not readable!")
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
 });
+
+/*browser.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension");
+    
+    
+        console.log("received message from popup: "+request.greeting);
+    
+        sendResponse({farewell: "I'm good, thank you popup!"});
+    });*/
+
+//console.log(document.body.innerHTML);
+//console.log(document.body.innerText);
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,32 @@
-console.log("Hello World!", browser);
+function handleResponse(message) {
+    console.log(`Message from the background script: ${message}`);
+}
+
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function notifyBackgroundPage(e) {
+  const sending = browser.runtime.sendMessage({
+    greeting: "hello from popup",
+  });
+  sending.then(handleResponse, handleError);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("summarize").addEventListener("click", notifyBackgroundPage);
+});
+
+document.getElementById("summarize").onclick = function(){
+    console.log("clicked button");
+
+    browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        if(tabs.length == 0){ 
+            console.log("could not send mesage to current tab");
+        }else{
+            browser.tabs.sendMessage(tabs[0].id, {greeting: "hello, how are you content script?"}, function(response) {
+                console.log("received message from content script: "+response.farewell);
+            });
+        }
+    });
+}
