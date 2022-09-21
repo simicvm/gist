f29a3b07b873f0b058c836d40bf7a0f1343901f0
Marko Simic <simic10@gmail.com>
2022-09-21 23:53:26 +0900

    Remove duplicate function

--- Shared (App)/ViewController.swift
+++ Shared (App)/ViewController.swift
@@ -78,17 +78,5 @@ class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMess
         }
 #endif
     }
-
-    override func beginRequest(with context: NSExtensionContext) {
-        print("test")
-        let item = context.inputItems[0] as! NSExtensionItem
-        let message = item.userInfo?[SFExtensionMessageKey]
-        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
-    
-        let response = NSExtensionItem()
-        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
-    
-        context.completeRequest(returningItems: [response], completionHandler: nil)
-    }
 }
 
